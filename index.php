e83e5ed2df27df9b9e653e984992b65988fcb1aa
sitiidatun <39979512+sitiidatun@users.noreply.github.com>
2018-06-07 09:31:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Template</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="wrap">
+		<div class="header">
+			<center>
+				<h1>SMK CENDEKIA LASEM</h1>
+				<P>Tutorial belajar membuat layout sederhana</P>
+			</center>
+		</div>
+	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="index.html">HOME</a></li>
+			<li><a href="profil.html">PROFIL</a></li>
+			<li><a href="gallery.html">GALLERY</a></li>
+			<li><a href="kontak.html">KONTAK</a></li>
+		</ul>
+	</div>
+	<div class="badan">
+		<div class="sidebar">
+		Berita
+		<ul>
+			<li><a href="lombaudinus.html">smk cendekia lasem juara 3 lomba desaibn di udinus</a></li>
+			<li><a href="upacara.html">upacara hari pendidikan nasional</a></li>
+			<li><a href="peresmian.html">peresmian teaching factory smk cendekia lasem</a></li>
+		</ul>
+			<div class="content">
+				<h1>Selamat Datang di Website SMK CENDEKIA LASEM</h1>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<div class="footer">
+			<center><b>&copy;SMK CENDEKIA LASEM 2018</b></center>
+			
+		</div>
+	</div>
+</body>
+</html>
